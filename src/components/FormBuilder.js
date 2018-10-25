88e5e7951143ea0f4118d6da460d8b73af0100ac
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-10-25 17:38:03 +0200

    tryinging to make IndexedDB work in react

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -1,23 +1,132 @@
 import React, {Component} from 'react';
 import Form from './Form';
 import _ from 'lodash';
-import data from './data';
-import idb from 'idb';
+import data  from './data';
 
-// await idb.open(…);
 
-const name = 'mydbname'
-const version = 1 //versions start at 1
-idb.open(name, version, upgradeDb => {})
+
 
 
 class FormBuilder extends Component{
   constructor(props){
     super(props);
     this.state={
-      form:data
+      form:''
     }
   }
+  componentDidMount(){
+            let self = '';
+            // all the variables to run our database
+            // let self;
+            console.log("selfjeden---",self);
+            var database, idb_request;
+            // request to open the specified database by name and version number
+            // if version number changes, the database is updated
+            idb_request = window.indexedDB.open("form-db", 1);
+            // if there is an error, tell the user
+            idb_request.addEventListener("error", function(event) {
+              alert("Could not open Indexed DB due to error: " + this.errorCode);
+            });
+            /* if the database you specified cannot be found or the version number
+            is old, you will need an upgrade to create the new database schema */
+// UPGRADEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+            idb_request.onupgradeneeded = function(event) {
+              /* Here we create a new object store called data, and give it an auto-
+              generated key path */
+              var storage = this.result.createObjectStore("formData", { autoIncrement: true });
+              // add an object to the "formData" objectStore with the key, "form"
+              storage.add(data, "form");
+              alert("Creating a new database!");
+            };
+            // if you successfully open the database use this callback function
+  // SUCCESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+            idb_request.onsuccess = (event)=> {
+              console.log("self---",self);
+
+
+              database = this.result;// store the database for later use
+              // now we are going to use some data from our database
+              console.log("new database",this.result.transaction('formData','readwrite').objectStore('formData').get("form"));
+              // window.indexedDB.deleteDatabase("form-db");
+              // database.close();
+
+              // return;
+
+
+              let storage = database.transaction("formData", "readwrite").objectStore("formData");
+
+              storage.get("form").onsuccess = (event)=> {
+                console.log("database.result",database.result);
+          //  that.setState({
+          //     form:self
+          //   })
+          self = this.result;
+                console.log("seljoijf---",self);
+                // background = document.body.style.backgroundColor = this.result.color;
+                // document.getElementById("date").innerHTML = this.result.date;
+                // presses = document.getElementById("presses").innerHTML = this.result.presses;
+                // this.result.date = new Date().toString();
+                // storage.put(this.result, "form");
+              };
+              console.log("storage=====", storage);
+              alert("Successfully opened database!");
+              database.transaction.oncomplete = function(){
+                    database.close();
+                }
+            };
+            console.log("ostatni self", self);
+
+            // this.setState({
+            //   form:self
+            // })
+  // FORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+            // // all the variables to run our application
+            // var buttons, background, presses;
+            // // get the array of buttons, which are just a elements:
+            // buttons = document.querySelectorAll("a");
+            // // set presses equal to zero (this will be reset if our database loads):
+            // presses = 0;
+            // // loop through all the buttons:
+            // for (let index = buttons.length - 1; index > -1; -- index) {
+            //   // add a click listener to each button:
+            //   buttons[index].addEventListener("click", function(event) {
+            //     // Clear the database if the X button is pressed:
+            //     if (database && this.innerHTML == "X") {
+            //       window.indexedDB.deleteDatabase("indexed-db");
+            //       database = undefined;
+            //       // set up html for the white screen after deleting database
+            //       document.getElementById("button-container").style.visibility = "hidden";
+            //       document.querySelector("h1").innerHTML = "You just deleted the database! Refresh the page to create a new one.";
+            //       document.querySelector("h1").style = "color:" + background;
+            //       document.body.style.backgroundColor = "#ffffff";
+            //       return;
+            //     } else if (database) { // if the database was established
+            //       presses ++;
+            //       // when a button is clicked, store its background color for saving:
+            //       background = this.style.backgroundColor;
+            //       // change the background color of the page to the button's color:
+            //       document.body.style.backgroundColor = background;
+            //       document.getElementById("presses").innerHTML = presses;
+            //       // save the new data to the database in the objectStore, "data"
+            //       var storage = database.transaction("data", "readwrite").objectStore("data");
+            //       // get returns the object pointed to by the key, "form"
+            //       storage.get("form").addEventListener("success", function(event) {
+            //         // this.result is the object "form" was pointing to
+            //         this.result.color = background;
+            //         this.result.presses = presses;
+            //         // put writes the changed object back to the "data" objectStore
+            //         storage.put(this.result, "form");
+            //       });
+            //     }
+            //   });
+            // }
+  }
+
+fetchedDataToForm(data){
+  this.setState({
+    form:data
+  })
+}
 
 removeForm(actionType, id){
   const initialData= this.state.form?this.state.form:[];
@@ -121,7 +230,7 @@ addForm(actionType,id,fatherType,fatherConditionValue){
 
 
   renderItems(form, layer) {
-    _.remove(form,(item)=>Object.keys(item).length === 0);
+    _.remove(form,(item)=>Object.keys(item).length === 0||typeof item !=="object");
     const i = layer===false||typeof layer !== "number"?0:layer+1;
       return form.map((item, index) => <div key={index}>
       <Form
@@ -139,12 +248,23 @@ g
   render(){
       const {form} = this.state;
       // console.log("data from render Fbuilder",form);
-    return(
-       <div>
-         {this.renderItems(form)}
-         <button onClick={this.addForm.bind(this,'add','',"new")}>Add form</button>
-       </div>
-    )
+      if(this.state.form){
+        return(
+           <div>
+             {this.renderItems(form)}
+             <div id="date"></div>
+             <div id="presses"></div>
+             <button onClick={this.addForm.bind(this,'add','',"new")}>Add form</button>
+           </div>
+        )
+      } else{
+        return(
+           <div>
+             <button onClick={this.addForm.bind(this,'add','',"new")}>Add form</button>
+           </div>
+        )
+      }
+
   }
 }
 
---
+++ src/components/workingDataDatabase.js
@@ -0,0 +1,136 @@
+
+    if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+    }
+
+    var database, idb_request;
+    idb_request = window.indexedDB.open("indexed-db",1);
+    idb_request.onerror = function(event) {
+      alert("Could not open Indexed DB due to error: " + this.errorCode);
+    };
+    /* if the database you specified cannot be found or the version number
+    is old, you will need an upgrade to create the new database schema */
+    console.log(database);
+    idb_request.upgradeneeded = function(event) {
+      console.log("duuuuuuuuuuuuuupa");
+      /* Here we create a new object store called data, and give it an auto-
+      generated key path */
+      // let db = request.result,
+      //          store = db.createObjectStore("FormBuilderStore",{
+      //            keyPath:"id"}),
+      //          index = store.createIndex("type","type",{unique: false});
+      //
+
+
+      // let storage = this.result.createObjectStore("data", { autoIncrement: true });
+
+      let db = idb_request.result;
+      let storage = db.createObjectStore("FormBuilderStore",{
+                 keyPath:"id"});
+      let index = storage.createIndex("type","type",{unique:false});
+      // add an object to the "data" objectStore with the key, "save-data"
+      storage.add(data);
+      console.log("data------", data);
+      // storage.add({ color: "#2793e8", date: "not found.", presses: "0" }, "save-data");
+      alert("Creating a new database!");
+    };
+    // if you successfully open the database use this callback function
+    idb_request.onsuccess = function(event) {
+      database = this.result
+      window.indexedDB.deleteDatabase("indexed-db");
+      database.close();
+
+
+
+      return;
+      // console.log(idb_request.result);
+      // console.log(window.indexedDB);
+      // let db = this.result;
+      // let tx = db.transaction("FormBuilderStore","readwrite");
+      // let storage = tx.objectStore('type');
+      // let index = storage.index('FormBuilderStore');
+      // //
+      // db.onerror = function(e) {
+      //   console.log("Error" + e.target.errorCode);
+      // }
+      //
+      // store.put({
+      //     id:234,
+      //     type:'text',
+      //     question:"second one",
+      //     conditionType:'',
+      //     conditionOption:'',
+      //     conditionValue:'',
+      //     subform:[]
+      // })
+      //
+      // let q1 = store.get(1);
+      // let qs = index.get("text");
+      //
+      // q1.onsuccess = function(){
+      //   console.log(q1.result);
+      //   console.log(q1.result.questionText);
+      // }
+      // qs.onsuccess = function(){
+      //   console.log(qs.result.questionText);
+      // }
+      //
+      // tx.oncomplete = function(){
+      //   db.close();
+      // }
+
+
+      database = this.result;// store the database for later use
+      // now we are going to use some data from our database
+      let storage = database.transaction("data", "readwrite").objectStore("data");
+      storage.get('save-data').onsuccess = function(event) {
+        console.log("FormBuilderStore:::::::",this.result);
+        // this.setState({from:this.result})
+        // background = document.body.style.backgroundColor = this.result.color;
+        document.getElementById("date").innerHTML = this.result.date;
+        // presses = document.getElementById("presses").innerHTML = this.result.presses;
+        this.result.date = new Date().toString();
+        storage.put(this.result, "save-data");
+      };
+      alert("Successfully opened database!");
+    };
+    // all the variables to run our application
+    var buttons, background, presses;
+    // get the array of buttons, which are just a elements:
+    buttons = document.querySelectorAll("a");
+    // set presses equal to zero (this will be reset if our database loads):
+    presses = 0;
+    // loop through all the buttons:
+    for (let index = buttons.length - 1; index > -1; -- index) {
+      // add a click listener to each button:
+      buttons[index].addEventListener("click", function(event) {
+        // Clear the database if the X button is pressed:
+        if (database && this.innerHTML == "X") {
+          window.indexedDB.deleteDatabase("indexed-db");
+          database = undefined;
+          // set up html for the white screen after deleting database
+          document.getElementById("button-container").style.visibility = "hidden";
+          document.querySelector("h1").innerHTML = "You just deleted the database! Refresh the page to create a new one.";
+          document.querySelector("h1").style = "color:" + background;
+          document.body.style.backgroundColor = "#ffffff";
+          return;
+        } else if (database) { // if the database was established
+          presses ++;
+          // when a button is clicked, store its background color for saving:
+          background = this.style.backgroundColor;
+          // change the background color of the page to the button's color:
+          document.body.style.backgroundColor = background;
+          document.getElementById("presses").innerHTML = presses;
+          // save the new data to the database in the objectStore, "data"
+          var storage = database.transaction("data", "readwrite").objectStore("data");
+          // get returns the object pointed to by the key, "save-data"
+          storage.get("save-data").addEventListener("success", function(event) {
+            // this.result is the object "save-data" was pointing to
+            this.result.color = background;
+            this.result.presses = presses;
+            // put writes the changed object back to the "data" objectStore
+            storage.put(this.result, "save-data");
+          });
+        }
+      });
+    }
